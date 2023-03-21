72ce41c0762558051040d43ae0d717fd3771d5a4
Yuv0101 <113253175+Yuv0101@users.noreply.github.com>
2023-03-21 22:14:56 +0530

    Create Way Too Long Words.cpp

---
+++ Way Too Long Words.cpp
@@ -0,0 +1,45 @@
+// Sometimes some words like "localization" or "internationalization" are so long that writing them many times in one text is quite tiresome.
+
+// Let's consider a word too long, if its length is strictly more than 10 characters. All too long words should be replaced with a special abbreviation.
+
+// This abbreviation is made like this: we write down the first and the last letter of a word and between them we write the number of letters between the first and the last letters. That number is in decimal system and doesn't contain any leading zeroes.
+
+// Thus, "localization" will be spelt as "l10n", and "internationalization» will be spelt as "i18n".
+
+// You are suggested to automatize the process of changing the words with abbreviations. At that all too long words should be replaced by the abbreviation and the words that are not too long should not undergo any changes.
+
+#include <bits/stdc++.h>
+#include <vector>
+#include <math.h>
+#include <string>
+#include <cstdlib>
+#define ll long long
+#define vll vector<long long>
+#define pb push_back
+#define pi pair<int, int>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        int len = s.length();
+        if (len <= 10)
+        {
+            cout << s << endl;
+        }
+        else
+        {
+            cout << s[0] << len - 2 << s[len - 1] << endl;
+        }
+    }
+}
